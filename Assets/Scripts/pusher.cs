eda0fae0c368cdb30f215f30599599f1cb235878
Ali <alice.krouse@gmail.com>
2019-04-18 13:05:52 -0500

    built push ability

--- Assets/Scripts/pusher.cs
+++ Assets/Scripts/pusher.cs
@@ -14,6 +14,8 @@ public class pusher : MonoBehaviour
     private float a;
 
     public float pushForce;
+
+    public LayerMask playerMask;
     
 	void Start ()
     {
@@ -26,29 +28,41 @@ public class pusher : MonoBehaviour
 	void Update ()
     {
         float size = transform.localScale.x;
-        size += Time.deltaTime * expandRate;
+        if (size < maxSize)
+            size += Time.deltaTime * expandRate;
         transform.localScale = new Vector2(size, size);
         
         a -= Time.deltaTime * fadeRate;
         sr.color = new Color(c.r, c.g, c.b, a);
 
-        if (size > maxSize)
-            GetComponent<CircleCollider2D>().enabled = false;
+        //if (size > maxSize)
+        //    GetComponent<CircleCollider2D>().enabled = false;
 
         if (a < 0)
         {
             Destroy(this.gameObject);
         }
+
+        Collider2D[] playersToPush = Physics2D.OverlapCircleAll(transform.position, size / 6, playerMask);
+        Debug.DrawRay(transform.position, Vector2.up * (size / 6), Color.red, 0.5f);
+        foreach (Collider2D col in playersToPush)
+        {
+            if (col.gameObject != mother)
+            {
+                Vector2 dir = col.gameObject.transform.position - transform.position;
+                col.gameObject.GetComponent<Rigidbody2D>().AddForce(dir * pushForce);
+            }
+        }
 	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log(collision.gameObject.name);
-        if (collision.gameObject.CompareTag("Player") && collision.gameObject != mother)
-        {
-            Debug.Log("pushing");
-            Vector2 direction = collision.gameObject.transform.position - transform.position;
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(direction * pushForce);
-        }
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    Debug.Log(collision.gameObject.name);
+    //    if (collision.gameObject.CompareTag("Player") && collision.gameObject != mother)
+    //    {
+    //        Debug.Log("pushing");
+    //        Vector2 direction = collision.gameObject.transform.position - transform.position;
+    //        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(direction * pushForce);
+    //    }
+    //}
 }
